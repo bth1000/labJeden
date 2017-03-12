b8313c9e1c4440fad35353879269dda44a42d45e
Łukasz Kocjan <ukasz Kocjan>
2017-03-12 09:53:09 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML CheckBox cb1, cb2, cb3; // button variable declaration
+    @FXML TextField oldPc;
+
+    public void ass(ActionEvent event) {
+        if (cb1.isSelected()){
+            System.out.println("Yeah!");
+        }
+    }
+
+    public void tits(ActionEvent event) {
+        String userInput=oldPc.getText();
+    }
+}
